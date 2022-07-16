eae5319fd404e923708cd24cac74596636f73964
realHoangHai <aflyingpenguin2lth@gmail.com>
2022-07-16 16:54:32 +0700

    update examples

--- _examples/main.go
+++ _examples/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	err := errors.New("wrong")
+	lo := logh.NewLogger(logh.DebugLevel, "hailth")
+	lo.Println(err)
+	// --------------------------------------------------
 	logh.Errorf("error: %v", err)
 	logh.Infof("info: success")
 	logh.Warnf("warn: %v", err)
